a4aa7567a565de89480876b17e773316af86dea8
Jaroslaw Abram <jarek.abram1@gmail.com>
2023-06-07 16:21:22 +0200

    Create board and improve styling

---
+++ BattleShips/Components/BoardCell.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BattleShips.Components;
+
+public class BoardCell : ComponentBase
+{
+  [Parameter] public RenderFragment ChildContent { get; set; }
+}
